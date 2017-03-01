d3c7ae17d8599862bed58a49f3d09657d08bd2f2
Sheffield, Gianna <sheffigm@miamioh.edu>
2017-03-01 17:58:09 -0500

    Application
    Pages and transitions implemented. Data currently hardcoded, must
    accept input.

---
+++ MTDDesign-moorenm3-cd6fbbf391ffa3e0c40e67983b1e7dc5f69470e7/MTDApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTDApp
+//
+//  Created by Brown, Drew J. on 10/30/16.
+//  Copyright © 2016 MTDGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
