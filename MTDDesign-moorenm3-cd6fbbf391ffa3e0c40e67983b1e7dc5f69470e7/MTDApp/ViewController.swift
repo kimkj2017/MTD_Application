fb372eab2a7938d114512b7109dc8cb4e514e045
Sheffield, Gianna <sheffigm@miamioh.edu>
2017-04-12 17:04:08 -0400

    It works kinda hooray!

--- MTDDesign-moorenm3-cd6fbbf391ffa3e0c40e67983b1e7dc5f69470e7/MTDApp/ViewController.swift
+++ MTDDesign-moorenm3-cd6fbbf391ffa3e0c40e67983b1e7dc5f69470e7/MTDApp/ViewController.swift
@@ -2,50 +2,133 @@
 //  ViewController.swift
 //  MTDApp
 //
-//  Created by Brown, Drew J. on 10/30/16.
-//  Copyright © 2016 MTDGroup. All rights reserved.
-//
 
 import UIKit
+import CoreBluetooth
+
+private let uuid: String = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+private let rx = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+private let tx = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
 class ViewController: UIViewController {
     
-    var simpleBluetoothIO: SimpleBluetoothIO!
-
+    var manager: CBCentralManager?
+    var peripheral: CBPeripheral?
+    var write: CBCharacteristic? {
+        didSet {
+            if let write = write {
+                if let test = "#0,00000,0000,0000,0000,0000,0,000,000,00,00,000,000,000,000,000,000,00000,00000,0000,0,0,00,0000,00000,0\n".data(using: .utf8) {
+                    
+                    if let read = read {
+                        peripheral?.readValue(for: read)
+                    }
+                    
+                    print("Sending: \(test.count)")
+                    peripheral?.writeValue(test, for: write, type: CBCharacteristicWriteType.withResponse)
+                    print("Wrote to device")
+                    
+                    
+                }
+            }
+        }
+    }
+    var read: CBCharacteristic?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        simpleBluetoothIO = SimpleBluetoothIO(serviceUUID: "6E400003-B5A3-F393-­E0A9-­E50E24DCCA9E", delegate: self)
+        manager = CBCentralManager(delegate: self, queue: nil)
     }
-    
-//    @IBAction func ledToggleButtonDown(_ sender: UIButton) {
-//        simpleBluetoothIO.writeValue(value: 1)
-//    }
-    
-//    @IBAction func ledToggleButtonUp(_ sender: UIButton) {
-//        simpleBluetoothIO.writeValue(value: 0)
-//    }
-
-    
-    /*
-    // Swifty friend advised to comment this out
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    */
-
-
 }
 
-extension ViewController: SimpleBluetoothIODelegate {
-    func simpleBluetoothIO(simpleBluetoothIO: SimpleBluetoothIO, didReceiveValue value: Int8) {
-        
-        // What we want to do with the data
-        if value > 0 {
-            view.backgroundColor = UIColor.yellow
-        } else {
-            view.backgroundColor = UIColor.black
+extension ViewController: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor descriptor: CBDescriptor, error: Error?) {
+        print("Wrote value")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        print("Updated value for: \(characteristic)")
+        if let data = characteristic.value {
+            print("Data: \(data)")
+            if let str = String(data: data, encoding: .utf8) {
+                print(str)
+                // Place data in struct?
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("Discovered services for: \(peripheral.name) - \(peripheral.services)")
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if let uuid = UUID(uuidString: tx) {
+                    let cb = CBUUID(nsuuid: uuid)
+                    if characteristic.uuid == cb {
+                        self.write = characteristic
+                    } else {
+                        self.read = characteristic
+                    }
+                }
+            }
         }
     }
 }
+
+extension ViewController: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("state changed")
+        switch central.state {
+        case .poweredOff:
+            print("powered off")
+        case .poweredOn:
+            print("powered on")
+            let cbuuid = CBUUID(string: uuid)
+            central.scanForPeripherals(withServices: [cbuuid], options: nil)
+        case .resetting:
+            print("resetting")
+        case .unauthorized:
+            print("unauthorized")
+        case .unsupported:
+            print("unsupported")
+        case .unknown:
+            print("unknown")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected to \(peripheral.name)")
+        peripheral.delegate = self
+        let cbuuid = CBUUID(string: uuid)
+        
+        //peripheral.writeValue(<#T##data: Data##Data#>, for: <#T##CBCharacteristic#>, type: <#T##CBCharacteristicWriteType#>)
+        peripheral.discoverServices([cbuuid])
+        //peripheral.discoverServices(nil)
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("Discovered: \(peripheral.name)")
+        self.peripheral = peripheral
+        central.connect(peripheral, options: nil)
+        //central.stopScan()
+    }
+    
+}
+
